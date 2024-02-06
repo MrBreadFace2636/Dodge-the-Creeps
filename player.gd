2c06f8f3bc7f57fcff30d29ed4ba0d60c819e08d
MrBreadFace2636 <157673290+MrBreadFace2636@users.noreply.github.com>
2024-02-05 20:20:04 -0900

    choose animations for the player based on input

--- player.gd
+++ player.gd
@@ -6,7 +6,7 @@ var screen_size # Size of the game window.
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,3 +25,17 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
